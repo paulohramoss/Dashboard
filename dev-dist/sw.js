bbb4a1c6e9b16d8008051cf6d062b54c463d3d1a
Paulo Ramos <pramosphdr548@gmail.com>
2026-02-06 08:49:17 -0300

    feat: Add service worker with Workbox for precaching and routing.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-38bb0eb2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s6ou8s8c8u"
+    "revision": "0.g7t3c5c3qb"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
