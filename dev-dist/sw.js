8036bf454fa3c2e4ea5490083c08f176f04dd4da
Paulo Ramos <pramosphdr548@gmail.com>
2026-02-18 22:09:14 -0300

    feat: Implement Workbox service worker and application versioning.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-38bb0eb2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3r4qtrn92i"
+    "revision": "0.fiacacg81"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
